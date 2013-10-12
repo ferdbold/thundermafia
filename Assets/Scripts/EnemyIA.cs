f37e95e30087a81b61adbfc26be4fbdce8af14c1
tikev230 <kevin.l.paquette@hotmail.com>
2013-10-12 07:19:35 -0400

    Script d'ennemi qui poursuit le joueur
    Mettre tag "Player" à l'objet qui est le joueur pour que ça fonctionne,
    sinon une exception sera lancé par GameInfo.

---
+++ Assets/Scripts/EnemyIA.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyIA : MonoBehaviour
+{
+	public float speed = 10;
+	public float distanceBeforeAttacking = 200;
+	
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		Move ();
+	}
+	
+	private void Move ()
+	{
+		Vector3 nextLocation = transform.position;
+		Vector3 test = GameInfo.GetPlayerLocation ();
+		float test2 = GameInfo.GetPlayerDistanceFromPoint (transform.position);
+		
+		if (GameInfo.GetPlayerDistanceFromPoint (transform.position) > distanceBeforeAttacking) {
+			nextLocation = Vector3.MoveTowards (transform.position, GameInfo.GetPlayerLocation (), -speed);
+			Vector3 move = transform.position - nextLocation;
+			move.y = 0;
+			transform.Translate (move);
+		}
+	}
+	
+	private void Attack()
+	{
+	}
+}
---
+++ Assets/Scripts/GameInfo.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameInfo : MonoBehaviour
+{
+	
+	/// <summary>
+	/// Gets the player location.
+	/// </summary>
+	/// <returns>
+	/// The player location.
+	/// </returns>
+	public static Vector3 GetPlayerLocation ()
+	{
+		GameObject[] go = GameObject.FindGameObjectsWithTag ("Player");
+		
+		foreach (GameObject player in go) {
+			return player.transform.position;
+		}
+		
+		throw new System.Exception("No player detected!");
+	}
+	
+	/// <summary>
+	/// Gets the player distance from point.
+	/// </summary>
+	/// <returns>
+	/// The player distance from a point.
+	/// </returns>
+	/// <param name='point'>
+	/// Point.
+	/// </param>
+	public static float GetPlayerDistanceFromPoint (Vector3 point)
+	{
+		Vector3 vec1, vec2;
+		vec1 = GetPlayerLocation ();
+		vec1.y = 0;
+		vec2 = point;
+		vec2.y = 0;
+		return Vector3.Distance (vec2, vec1);
+	}
+	
+	/// <summary>
+	/// Gets the angle between player and a point.
+	/// </summary>
+	/// <returns>
+	/// The angle between player and point.
+	/// </returns>
+	/// <param name='point'>
+	/// Point.
+	/// </param>
+	public static float GetAngleBetweenPlayerAndPoint(Vector3 point)
+	{
+		return Vector3.Angle (point, GetPlayerLocation ());
+	}
+}
