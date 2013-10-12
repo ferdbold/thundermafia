7f1e6ba0d340f047ae7c7378a54d7a1999dff354
Eiz00 <guilltrem@gmail.com>
2013-10-12 05:53:43 -0400

    fsfdsfsfdsdfsfdsfddsfsfds:wq adssdfsdfsfds sdffdfgdgfhghghjghjghj;::ef=sqqwedgfjghjopvbvbnvnb^¸¸

---
+++ Assets/RingSpawner.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RingSpawner : MonoBehaviour {
+	
+	public GameObject circle;
+	private int z = 6;
+	
+	// Use this for initialization
+	void Start () {
+		
+		while (z < 100)
+		{
+			Instantiate(circle, new Vector3(0, 0 , this.z), circle.transform.rotation);
+			z += 5;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	public void nextStage()
+	{
+		Instantiate(circle, new Vector3(0, 0, this.z), circle.transform.rotation);
+		z += 5;
+	}
+}
