485a9d4863d3d663a246083c1b1c7c24eeefb1cb
Eiz00 <guilltrem@gmail.com>
2013-10-12 06:35:43 -0400

    Ajout cercles

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControl : MonoBehaviour {
+	public float polly = -10;
+
+	void Update () {
+		transform.position = new Vector3(transform.parent.position.x * 0.5f, transform.parent.position.y * 0.5f, polly);
+	}
+}
