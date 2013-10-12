45756009211e741a858489598f899c9c3cac3cf5
Frédéric <bolducfrederic@gmail.com>
2013-10-12 18:25:36 -0400

    Fix scripts UI

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIManager : MonoBehaviour {
+	private TempoWidget _tempoWidget;
+	public TempoWidget tempo {
+		get { return _tempoWidget; }
+	}
+	
+	void Start () {
+		_tempoWidget = transform.GetComponentInChildren<TempoWidget>();
+	}
+}
