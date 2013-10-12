3f7a9e8e436fcf57d6582d0c5d6e872474f3226f
GuillaumeFay <guillaume.fay@u-psud.fr>
2013-10-12 13:07:49 -0400

    MenuPrincipal

---
+++ Assets/Curseur.js
@@ -0,0 +1,12 @@
+﻿#pragma strict
+
+var tailleEntrer : float = 45;
+var tailleSortie : float = 100;
+ 
+function OnMouseEnter() {
+    guiText.fontSize = tailleEntrer;
+}
+ 
+function OnMouseExit() {
+    guiText.fontSize = tailleSortie;
+}
