6423dd6455cca900e6e4aeff3c5818e5ce436a54
Eiz00 <guilltrem@gmail.com>
2013-10-13 13:32:07 -0400

    Animation tres rigide, mais fonctionnelle.

---
+++ Assets/C_C_C_ComboBreaker.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class C_C_C_ComboBreaker : MonoBehaviour {
+	
+	public GameObject powPrefab;
+	public GameObject player;
+	public Texture texture;
+	public GameObject animateSpriteSheetPrefab;
+	
+	private AnimateSpriteSheet animateSpriteSheet;
+	
+	void Start () 
+	{
+		this.animateSpriteSheet = GameObject.Find("AnimateSpriteSheet").GetComponent<AnimateSpriteSheet>();
+	}
+	
+	
+	void Update () {
+	
+		if (Input.GetKeyDown(KeyCode.Space))
+		{				
+			Instantiate(this.animateSpriteSheetPrefab, new Vector3(0, 0 , this.player.transform.position.z + 1), animateSpriteSheetPrefab.transform.rotation);
+			animateSpriteSheet.Animate(this.player.transform.position.x, this.player.transform.position.y, this.player.transform.position.z + 1);
+		}
+	}
+}
---
+++ Assets/Sprite Sheet/AnimateSpriteSheet.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimateSpriteSheet : MonoBehaviour
+{
+    public int Columns = 8;
+    public int Rows = 9;
+    public float FramesPerSecond = 1f;
+    public bool RunOnce = true;
+ 
+	public Texture texture;
+	public GameObject powPrefab;
+	
+	private GameObject prefab;
+	
+    public float RunTimeInSeconds
+    {
+        get
+        {
+            return ( (1f / FramesPerSecond) * (Columns * Rows) );
+        }
+    }
+ 
+    private Material materialCopy = null;
+ 
+    void Start()
+    {		
+		materialCopy = new Material(powPrefab.renderer.material);
+        powPrefab.renderer.material = materialCopy;
+ 
+        Vector2 size = new Vector2(1f / Columns, 1f / Rows);
+        powPrefab.renderer.material.SetTextureScale("_MainTex", size);
+    }
+	
+	void Update() {
+		if (Input.GetKeyUp (KeyCode.H)) {
+			GameObject.Instantiate(powPrefab, transform.position, transform.rotation);	
+		}
+	}		
+	
+	public void Animate(float x, float y, float z)
+	{	
+		prefab = (GameObject)Instantiate(powPrefab, new Vector3(x, y , z), powPrefab.transform.rotation);
+		
+		StartCoroutine(UpdateTiling());
+	}
+	
+	private IEnumerator UpdateTiling()
+    {
+        float x = 0f;
+        float y = 0f;
+        Vector2 offset = Vector2.zero;
+ 
+        while (true)
+        {
+            for (int i = Rows-1; i >= 0; i--) // y
+            {
+                y = (float) i / Rows;
+ 
+                for (int j = 0; j <= Columns-1; j++) // x
+                {
+                    x = (float) j / Columns;
+ 
+                    offset.Set(x, y);
+					
+                    prefab.renderer.material.SetTextureOffset("_MainTex", offset);
+                    yield return new WaitForSeconds(0.005f / FramesPerSecond);
+                }
+            }
+ 
+            if (RunOnce)
+            {
+				prefab.transform.Translate(new Vector3(0, 0, -100)); //shame
+                yield break;
+            }
+        }
+    }
+}
