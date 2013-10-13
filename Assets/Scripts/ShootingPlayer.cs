229f621a6e4c4b8581153af7c3c59dc76d8a249f
GuillaumeFay <guillaume.fay@u-psud.fr>
2013-10-13 16:20:38 -0400

    Shoot

--- Assets/Scripts/ShootingPlayer.cs
+++ Assets/Scripts/ShootingPlayer.cs
@@ -8,7 +8,8 @@ public class ShootingPlayer : MonoBehaviour {
 	private float nextBullet = 0.0f;	
 	private Vector3 mousePos;
 	private float posX;
-	private float posY;
+	public float maxRotation = 360;
+	//private float posY;
 
 	// Use this for initialization
 	void Start () {
@@ -27,23 +28,35 @@ public class ShootingPlayer : MonoBehaviour {
 		angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;    		
 		transform.rotation = Quaternion.Euler(0.0f, angle, 0.0f);
 		*/
-		
+		/*
 		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
 		
 		RaycastHit hit;
-		
+
 		if (Physics.Raycast(ray, out hit)) {
 			
 			posX = hit.transform.position.x;
 			
 			posY = hit.transform.position.y;
 			
-			Vector3 target = new Vector3(posX, posY, transform.position.z);
+			//Vector3 target = new Vector3(posX, posY, transform.position.z);
 			
-			transform.LookAt(target);
+			//transform.LookAt(target);
 			
-			if (Input.GetButton("Fire1") && Time.time > nextBullet){
-					
+		 */
+			
+		mousePos = Input.mousePosition;
+		
+		Vector3 the_return = Vector3.RotateTowards (transform.forward, mousePos, Mathf.Deg2Rad * maxRotation * Time.deltaTime, 1);
+		
+		transform.rotation = Quaternion.LookRotation (the_return);
+		
+		//transform.rotation.LookRotation(Quaternion.Euler(mousePos));
+		
+		
+		if (Input.GetButton("Fire1") && Time.time > nextBullet){
+				
+				//transform.rotation = Quaternion.Euler(90, 0, 0);	
 				nextBullet = Time.time + fireRate;
 				GameObject tire = (GameObject)GameObject.Instantiate (bullet, transform.position, transform.rotation);
 				tire.gameObject.tag="PlayerProjectile";
@@ -53,5 +66,4 @@ public class ShootingPlayer : MonoBehaviour {
 		}
 
 		
-	}
 }
