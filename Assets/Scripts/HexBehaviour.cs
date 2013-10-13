62daaef6648eda28d22466c13e94b31b5839f2bb
Frédéric <bolducfrederic@gmail.com>
2013-10-13 13:24:02 -0400

    More hexagons is always good

---
+++ Assets/Scripts/HexBehaviour.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HexBehaviour : MonoBehaviour {
+	private float rotationFactor;
+	
+	void Start () {
+		rotationFactor = Random.Range(-100, 100);
+	}
+	
+	void Update () {
+		transform.Rotate(new Vector3(0, rotationFactor * Time.deltaTime, 0));
+	}
+}
