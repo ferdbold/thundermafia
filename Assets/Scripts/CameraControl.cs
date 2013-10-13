b97c30bd28688dd60041a197295fa8cb48806682
Frédéric <bolducfrederic@gmail.com>
2013-10-13 11:45:13 -0400

    Plus visuel

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -2,10 +2,12 @@
 using System.Collections;
 
 public class CameraControl : MonoBehaviour {
+	public float followFactor = 0.5f;
+	
 	void Update () {
 		Vector3 buffer = transform.position;
-		buffer.x = transform.parent.position.x * 0.5f;
-		buffer.y = transform.parent.position.y * 0.5f;
+		buffer.x = transform.parent.position.x * followFactor;
+		buffer.y = transform.parent.position.y * followFactor;
 		transform.position = buffer;
 	}
 }
